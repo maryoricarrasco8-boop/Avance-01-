99afe9ffef43e73f0f8ae275a6fc6d9819707cb7
maryoricarrasco8-boop <maryoricarrasco8@gmail.com>
2025-09-06 09:38:11 -0500

    Initialize Spring Boot WebFlux project with reactive MongoDB and Lombok

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+
+@Data
+@Document
+public class User {
+    @Id
+    private String id;
+    private String name;
+}
